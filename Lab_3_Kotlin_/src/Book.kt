f79a52484c03bec989716c0c633f77f76ed1b066
Ibnodam <144240270+Ibnodam@users.noreply.github.com>
2024-11-17 23:38:30 +0200

    Update Book.kt

--- Lab_3_Kotlin_/src/Book.kt
+++ Lab_3_Kotlin_/src/Book.kt
@@ -1,10 +1,13 @@
+
 class Book {
 
-    var name: String = ""
-    var sheets: Int = 0
-    var price: Double = 0.0
+    private var _name: String = "Unknown book"
+    private var _sheets: Int = 0
+    private var _price: Double = 0.0
 
     constructor() {
+        // Это конструктор по умолчанию, моё задание требовало по варианту его наличие
+
         this.name = "Unknown book"
         this.sheets = 0
         this.price = 0.0
@@ -16,17 +19,48 @@ class Book {
         this.price = price
     }
 
+    var name: String
+        get() = _name
+        set(value) {
+            _name = value.trim()
+        }
+
+    var sheets: Int
+        get() = _sheets
+        set(value) {
+            if (value >= 0) {
+                _sheets = value
+            } else {
+                throw IllegalArgumentException("Number of sheets has to be positive Integer!")
+            }
+        }
+
+    var price: Double
+        get() = _price
+        set(value) {
+            if (value >= 0) {
+                _price = value
+            } else {
+                throw IllegalArgumentException("Price has to be positive Double number!")
+            }
+        }
+
     fun averageSheetPrice(): Double {
-        if(this.sheets > 0) return this.price / this.sheets
-        else{
+        return if (this.sheets > 0) {
+            this.price / this.sheets
+        } else {
             println("The number of sheets in book has to be more than 0")
-            return -404.00}
-    }
-    fun startsWith(){
-        if(this.name.startsWith("Программирование", ignoreCase = false)) this.price *= 2
+            -404.00
+        }
     }
 
-    fun getInfo(): String{
+    fun startsWith() {
+        if (this.name.startsWith("Программирование", ignoreCase = false)) {
+            this.price *= 2
+        }
+    }
+
+    fun getInfo(): String {
         return "Title: $name | Sheets: $sheets | Price: $price"
     }
 
